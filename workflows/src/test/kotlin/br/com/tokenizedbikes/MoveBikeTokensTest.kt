36befd4e74e97dc82fdb9bb350daa28ddb6045d3
bruno cardoso <bruno.cardoso@7comm.com.br>
2022-04-07 18:43:41 -0300

    move tokens teste ampliado

--- workflows/src/test/kotlin/br/com/tokenizedbikes/MoveBikeTokensTest.kt
+++ workflows/src/test/kotlin/br/com/tokenizedbikes/MoveBikeTokensTest.kt
@@ -7,7 +7,6 @@ import br.com.tokenizedbikes.models.BikeColor
 import br.com.tokenizedbikes.models.BikeColorEnum
 import br.com.tokenizedbikes.models.BikeModelDTO
 import br.com.tokenizedbikes.states.BikeTokenState
-import com.r3.corda.lib.tokens.workflows.flows.move.MoveTokensFlow
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
 import org.junit.Test
@@ -18,7 +17,7 @@ import kotlin.test.assertNotNull
 class MoveBikeTokensTest: FlowTests() {
 
     @Test
-    fun `Token Move Vanilla Test`() {
+    fun `Token Move - Vanilla Test`() {
         val bikeColor = BikeColor(
             mainColor = BikeColorEnum.GREEN,
             colorDescription = "None",
@@ -68,11 +67,70 @@ class MoveBikeTokensTest: FlowTests() {
 
         assertEquals(bikeStateAndRef4, bikeStateAndRef5)
 
-        val moveTokenTokenFlow2 = MoveBikeTokenFlow.MoveBikeTokenInitiatingFlow("21312AAAs", nodeC.info.legalIdentities.first())
+        val moveTokenTokenFlow2 = MoveBikeTokenFlow.MoveBikeTokenInitiatingFlow("21312AAAs", nodeB.info.legalIdentities.first())
 
         assertThrows<Exception> {
             nodeB.runFlow(moveTokenTokenFlow2).getOrThrow()
         }
+    }
+
+    @Test
+    fun `Token Move - PING-PONG Test`() {
+        val bikeColor = BikeColor(
+            mainColor = BikeColorEnum.GREEN,
+            colorDescription = "None",
+            isCustomColor = false
+        )
+
+        val bikeDTO = BikeModelDTO(
+            brand = "Cannondale",
+            modelName = "Habit Carbon 1",
+            percentOfConservation = 200.00,
+            year = 2021,
+            color = bikeColor,
+            bikeImageURL = "https://bikeshopbarigui.com.br/upload/estoque/produto/principal-6206-604e687b067f4-cannondale-habit-1.jpg",
+            serialNumber = "21312AAAs",
+            dollarPrice = 200.00,
+            isNew = true
+        )
+
+        val bikeFlow = CreateBikeTokenFlow(bikeDTO)
+        val result = nodeA.runFlow(bikeFlow).getOrThrow()
+
+        val bikeStateAndRef = nodeA.services.vaultService.queryBy<BikeTokenState>().states
+            .filter { it.state.data.serialNumber == result.bikeSerialNumber }[0]
+
+        assertNotNull(bikeStateAndRef)
+        assertNotNull(bikeStateAndRef.state.data)
+        assertEquals(bikeStateAndRef.state.data.serialNumber, bikeDTO.serialNumber)
+
+        val issueBikeFlow = IssueBikeTokenFlow("21312AAAs", nodeB.info.legalIdentities.first())
+        val result2 = nodeA.runFlow(issueBikeFlow).getOrThrow()
+
+        val bikeStateAndRef2 = nodeB.services.vaultService.queryBy<BikeTokenState>().states
+            .filter { it.state.data.serialNumber == result2.bikeSerialNumber }[0]
+
+        assertNotNull(bikeStateAndRef2)
+        assertNotNull(bikeStateAndRef2.state.data)
+        assertEquals(bikeStateAndRef2.state.data.serialNumber, bikeDTO.serialNumber)
+
+        val moveTokenTokenFlow = MoveBikeTokenFlow.MoveBikeTokenInitiatingFlow("21312AAAs", nodeC.info.legalIdentities.first())
+        val result3 = nodeB.runFlow(moveTokenTokenFlow).getOrThrow()
+
+        val bikeStateAndRef4 = nodeB.services.vaultService.queryBy<BikeTokenState>().states
+            .filter { it.state.data.serialNumber == result3.bikeSerialNumber }[0]
+
+        val bikeStateAndRef5 = nodeC.services.vaultService.queryBy<BikeTokenState>().states
+            .filter { it.state.data.serialNumber == result3.bikeSerialNumber }[0]
+
+        assertEquals(bikeStateAndRef4, bikeStateAndRef5)
+
+        val moveTokenTokenFlow2 = MoveBikeTokenFlow.MoveBikeTokenInitiatingFlow("21312AAAs", nodeB.info.legalIdentities.first())
+
+        nodeC.runFlow(moveTokenTokenFlow2).getOrThrow()
+
+        val moveTokenTokenFlow3 = MoveBikeTokenFlow.MoveBikeTokenInitiatingFlow("21312AAAs", nodeC.info.legalIdentities.first())
+        nodeB.runFlow(moveTokenTokenFlow3).getOrThrow()
     }
 
 }
